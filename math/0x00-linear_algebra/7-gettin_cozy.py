d05058e4018fc3f37a69b2c417fff292179d16d1
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-15 00:00:07 +0000

    Added concatenate rows

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -4,10 +4,11 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """Function that concatenates two matrices along a specific axis"""
-    #!/usr/bin/env python3
-"""Module to execute functions"""
-
-
-def cat_matrices2D(mat1, mat2, axis=0):
-    """Function that concatenates two matrices along a specific axis"""
-    
+    if axis == 0:
+        return mat1 + mat2
+    elif axis == 1:
+        for i in mat1:
+            for j in mat2:
+                return i + j
+    else:
+        return None
