f89965533a723ca1bcee728492a509917091b4fc
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-15 14:52:01 +0000

    Fixed shape function

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -4,19 +4,10 @@
 
 def matrix_shape(matrix):
     """Function that calculates the shape of a matrix"""
-    rows = 0
-    columns = 0
-    count = 0
     shape = []
-    for i in matrix:
-        rows = rows + 1
-        for j in i:
-            columns = len(i)
-            if type(j) == list:
-                for k in j:
-                    count = len(j)
-    shape.append(rows)
-    shape.append(columns)
-    if count > 0:
-        shape.append(count)
+    shape.append(len(matrix))
+    shape.append(len(matrix[0]))
+    i = matrix[0]
+    if type(i[0]) == list:
+        shape.append(len(i[0]))
     return shape
