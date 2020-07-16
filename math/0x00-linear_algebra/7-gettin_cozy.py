cd5b93b2f59ae8a55e39ac20616f647e2edd567a
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-16 02:43:15 +0000

    Fixed concatenation function

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -4,12 +4,16 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """Function that concatenates two matrices along a specific axis"""
+    m1 = [x[:] for x in mat1]
+    m2 = [y[:] for y in mat2]
     concatenate = []
-    if axis == 0:
-        return mat1 + mat2 
-    elif axis == 1:
-        for i, j in zip(mat1, mat2):
+
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
+        return m1 + m2
+
+    elif axis == 1 and len(mat1) == len(mat2):
+        for i, j in zip(m1, m2):
             concatenate.append(i + j)
-            return concatenate
+        return concatenate
     else:
         return None
