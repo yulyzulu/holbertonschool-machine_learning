4926ba110c46404a730b33d34a813dda009b956b
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-24 22:16:54 +0000

    Fixed derivative function

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -13,6 +13,9 @@ def poly_derivative(poly):
             return [0]
         else:
             for i, j in zip(range(0, length), poly):
-                der = i * j
-                coefficients.append(der)
+                if type(j) != int and type(j) != float:
+                    return None
+                else:
+                    der = i * j
+                    coefficients.append(der)
         return coefficients[1:]
