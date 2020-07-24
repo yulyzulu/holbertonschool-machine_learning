2c6135cb8c74ea363ea831263ad9adf5befc9a0f
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-24 18:47:50 +0000

    Added summation function

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Module to execute functions"""
 
+
 def summation_i_squared(n):
     """Function that calculates summation"""
-    sum = 1
     if type(n) is not int:
         return None
     else:
-#        sum = n * ((n + 1)/2)**2
-        suma = (n * (n + 1) * ((2 * n) + 1)) / 6
-        return suma
+        summation = (n * (n + 1) * ((2 * n) + 1)) / 6
+        return summation
