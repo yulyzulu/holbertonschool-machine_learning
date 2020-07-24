e78aae3cfa16da72fd2204b7fdfea1abf8c3dcd2
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-24 22:48:47 +0000

    Added integral function

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module to execute functions"""
+
+
+def poly_integral(poly, C=0):
+    """Function that calculates the integral of a polinomial"""
+    length = len(poly)
+    coefficients = []
+    if type(poly) != list or type(C) != int:
+        return None
+    else:
+        coefficients.append(C)
+        for i, j in zip(range(1, length + 1), poly):
+            integral = (j / i) + C
+            coefficients.append(integral)
+        return coefficients
