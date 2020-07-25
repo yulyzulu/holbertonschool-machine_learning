de9f2cede9d8c0ec8d0027d26ec9318ce3bb2856
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-25 15:48:26 +0000

    Added conditionals to poly list

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,7 +6,7 @@ def poly_derivative(poly):
     """Function that calculates the derivative of a polinomial"""
     length = len(poly)
     coefficients = []
-    if type(poly) != list:
+    if type(poly) != list or poly is None or poly == []:
         return None
     else:
         if length == 1:
