c2d3f42d9f7b7563f5219a06fc3096f4d6e43862
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-25 15:44:41 +0000

    Added n > 0 conditional

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,8 +4,9 @@
 
 def summation_i_squared(n):
     """Function that calculates summation"""
-    if type(n) != int and type(n) != float:
-        return None
-    else:
-        summation = (n * (n + 1) * ((2 * n) + 1)) / 6
-        return int(summation)
+    if n > 0:
+        if type(n) != int and type(n) != float:
+            return None
+        else:
+            summation = (n * (n + 1) * ((2 * n) + 1)) / 6
+            return int(summation)
