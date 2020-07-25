956cf19204b42778ae5aa7c11e16c5b36009d610
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-25 20:06:03 +0000

    Update function

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,16 +6,16 @@ def poly_derivative(poly):
     """Function that calculates the derivative of a polinomial"""
     length = len(poly)
     coefficients = []
-    if type(poly) != list or length == 0:
+    if type(poly) is not list or length == 0:
         return None
     else:
         if length == 1:
             return [0]
         else:
-            for i in range(1, length):
-                if type(poly[i]) != int and type(poly[i]) != float:
+            for i, j in zip(range(0, length), poly):
+                if type(j) is not int and type(j) is not float:
                     return None
                 else:
-                    der = i * poly[i]
+                    der = i * j
                     coefficients.append(der)
-        return coefficients
+        return coefficients[1:]
