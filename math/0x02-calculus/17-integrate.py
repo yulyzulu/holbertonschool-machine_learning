0f2285e48db4a3df5968b0c8012b027752490f0c
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-25 20:36:45 +0000

    Added float conditional C

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,23 +4,25 @@
 
 def poly_integral(poly, C=0):
     """Function that calculates the integral of a polinomial"""
-    length1 = len(poly)
+    length = len(poly)
     coefficients = []
-    if type(poly) != list or poly == [] or type(C) != int:
+    if type(poly) != list or poly == []:
         return None
-    else:
-        coefficients.append(C)
-        for num in reversed(poly):
-            if num == 0:
-                poly.pop(num)
-            else:
-                break
-        for i, j in zip(range(1, length1 + 1), poly):
-            integral = j / i
-            if type(j) is not int and type(j) is not float:
-                return None
-            if int(integral) == integral:
-                coefficients.append(int(integral))
-            else:
-                coefficients.append(integral)
-        return coefficients
+    if type(C) != int and type(C) != float:
+        return None
+
+    coefficients.append(C)
+    for num in reversed(poly):
+        if num == 0:
+           poly.pop(num)
+        else:
+            break
+    for i, j in zip(range(1, length + 1), poly):
+        integral = j / i
+        if type(j) is not int and type(j) is not float:
+            return None
+        if int(integral) == integral:
+            coefficients.append(int(integral))
+        else:
+            coefficients.append(integral)
+    return coefficients
