a03880fcd205632977f7c6f43cab11a7b2f9cf40
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-29 17:17:03 +0000

    Fixed pdf function

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -43,16 +43,17 @@ class Normal:
     def pdf(self, x):
         """Method that calculates the value of the PDF for
             a given x-value"""
-        p1 = (1 / (self.stddev * ((2 * self.pi) ** (1/2))))
-        p2 = (self.e ** -(((x - self.mean) ** 2) / 2 * (self.stddev ** 2)))
+        p1 = (1 / (self.stddev * (2 * self.pi)**(1/2)))
+        p2 = self.e**(-1/2 * ((x - self.mean) / (self.stddev))**2)
         return p1 * p2
 
     def err(self, x):
         """Method that calculates the error"""
-        error = (2 / (self.pi ** (1/2))) * (x - (x**3)/3 + (x**5)/10 - (x**7)/42 + (x**9)/216)
-        return error
+        err1 = (2 / (self.pi ** (1/2)))
+        err2 = (x - (x**3)/3 + (x**5)/10 - (x**7)/42 + (x**9)/216)
+        return err1 * err2
 
     def cdf(self, x):
         """Method that calculates the value of the CDF for a given x-value"""
-        c = 1/2 * (1 + self.err((x - self.mean) / (self.stddev * (2 ** (1/2)))))
+        c = 1/2 * (1 + self.err((x - self.mean)/(self.stddev * (2 ** (1/2)))))
         return c
