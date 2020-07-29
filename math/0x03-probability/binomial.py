0c42857a73a8b4d0f02232f786181d177d6bd0c0
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-29 20:38:24 +0000

    Added binomial distribution

---
+++ math/0x03-probability/binomial.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Binomial distribution"""
+
+
+class Binomial:
+    """Binomial class"""
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """Constructor Binomial class"""
+
+        if data is not None:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) <= 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                mean = sum(data) / len(data)
+                suma = 0
+                for i in data:
+                    val = (i - mean) ** 2
+                    suma = suma + val
+                variance = suma / (len(data) - 1)
+
+                self.p = 1 - (variance / mean)
+                self.n = int(mean / self.p)
+                self.p = mean / self.n
+        else:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            else:
+                self.n = float(n)
+            if p < 0 or p > 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+            else:
+                self.p = float(p)
