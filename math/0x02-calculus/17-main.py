52d1814eb87590fa292d5e05a72cbc10faf37cfe
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-29 22:27:53 +0000

    Added main

--- math/0x02-calculus/17-main.py
+++ math/0x02-calculus/17-main.py
@@ -1,18 +1,4 @@
 #!/usr/bin/env python3
 poly_integral = __import__('17-integrate').poly_integral
-poly = [5, 3, 0, 1]
+poly = None
 print(poly_integral(poly))
-poly = []
-print(poly_integral(poly))
-poly = "sdfsdf"
-print(poly_integral(poly))
-poly = [5, 3, 0, 1]
-print(poly_integral(poly, 7))
-poly = [5, 3, 0, 1]
-print(poly_integral(poly, "assssd"))
-poly = [5]
-print(poly_integral(poly))
-poly = [0, 0, 0, 0]
-print(poly_integral(poly, 3))
-poly = [0, 0, 0, 0]
-print(poly_integral(poly, 0))
