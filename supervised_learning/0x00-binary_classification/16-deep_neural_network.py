388df387666933b94a00060e2f2342ba3ffb317e
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-06 16:04:13 +0000

    Added layers len 0 conditional

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -15,7 +15,7 @@ class DeepNeuralNetwork:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         self.nx = nx
-        if type(layers) is not list:
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
         self.layers = layers
