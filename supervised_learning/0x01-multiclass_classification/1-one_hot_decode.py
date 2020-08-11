95ef27492d3fe590fb7b36835cdb01370937524f
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-11 14:55:33 +0000

    Added conditional shape

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -8,5 +8,7 @@ def one_hot_decode(one_hot):
     """Function that converts a one-hot matrix into a vector of labels"""
     if type(one_hot) is not np.ndarray or len(one_hot) == 0:
         return None
+    if len(one_hot.shape) != 2:
+        return None
     else:
         return np.argmax(one_hot, axis=0)
