e4831a3a15332b7c6ee646c44ac149020dc1e6b1
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-11 14:29:12 +0000

    Change conditionals

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """Function that converts a numeric label vector into a one-hot matrix"""
-    if np.max(Y) > classes:
+    if np.max(Y) > classes or len(Y) == 0:
         return None
     else:
         one = np.transpose(np.eye(classes)[Y])
