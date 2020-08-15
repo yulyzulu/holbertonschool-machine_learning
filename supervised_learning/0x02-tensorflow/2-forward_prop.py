94c048c475cac7264a1bde25e861ce4654f16c2f
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-15 15:33:03 +0000

    Added forward propagation function

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -8,4 +8,8 @@ def forward_prop(x, layer_sizes=[], activations=[]):
     """Function that creates the forward propagation
         graph for the Neural Network"""
     create_layer = __import__('1-create_layer').create_layer
-    
+    layers = len(layer_sizes)
+    prediction = x
+    for i in range(layers):
+        prediction = create_layer(prediction, layer_sizes[i], activations[i])
+    return prediction           
