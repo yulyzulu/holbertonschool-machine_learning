b899f815a21239915158a043567a7002a35bf51e
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-15 16:31:18 +0000

    Added loss function

---
+++ supervised_learning/0x02-tensorflow/4-calculate_loss.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Loss"""
+
+import tensorflow as tf
+
+
+def calculate_loss(y, y_pred):
+    """Function that calculates the softmax cross-entropy
+        loss of a predicction"""
+    loss = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=y_pred)
+    return loss
