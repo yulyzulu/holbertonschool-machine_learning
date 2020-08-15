cdcb8c7c11429f7f948c3a0894d9027af3724a98
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-15 14:00:25 +0000

    Organice the create layer function

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -10,6 +10,6 @@ def create_layer(prev, n, activation):
                             units=n,
                             activation=activation,
                             kernel_initializer=tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG"),
-                            bias_initializer=tf.zeros_initializer(),
+#                            bias_initializer=tf.zeros_initializer(),
                             name="layer")
     return layer
