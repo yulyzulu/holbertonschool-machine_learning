417819547145be0ffe7e9eb6aecf3afe7cbc833c
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-15 23:03:10 +0000

    Fixed pycodestyle

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -3,14 +3,15 @@
 
 import tensorflow as tf
 
-
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
 create_placeholders = __import__('0-create_placeholders').create_placeholders
 create_train_op = __import__('5-create_train_op').create_train_op
-forward_prop = __import__('2-forward_prop').forward_prop           
+forward_prop = __import__('2-forward_prop').forward_prop
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
+          activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """Function that builds, trains, and saves a neural network classifier"""
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     y_pred = forward_prop(x, layer_sizes, activations)
@@ -26,24 +27,26 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
 
 #    init_op = tf.initializers.global_variables()
     saver = tf.train.Saver()
-    
+
     with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
+        sess.run(tf.global_variables_initializer())
         step = 0
         for i in range(iterations + 1):
-            cost, acc = sess.run([loss, accuracy], feed_dict={x:X_train, y:Y_train})
-            cost2, acc2 = sess.run([loss, accuracy], feed_dict={x:X_valid, y:Y_valid})
+            cost, acc = sess.run([loss, accuracy],
+                                 feed_dict={x: X_train, y: Y_train})
+            cost2, acc2 = sess.run([loss, accuracy],
+                                   feed_dict={x: X_valid, y: Y_valid})
 
             if i == 0 or i == step or i == iterations:
                 print('After', i, 'iterations:')
                 print('\tTraining Cost:', cost)
                 print('\tTraining Accuracy:', acc)
                 print('\tValidation Cost:', cost2)
-                print('\tValidation Accuracy:', acc2) 
+                print('\tValidation Accuracy:', acc2)
                 step = step + 100
 
             if i != iterations:
-                sess.run([train_op], feed_dict={x:X_train, y:Y_train})
+                sess.run([train_op], feed_dict={x: X_train, y: Y_train})
 
         path = saver.save(sess, save_path)
         return path
