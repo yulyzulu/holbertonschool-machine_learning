2b2da9d21cacb8b9ef735eacdd8e5aadfcfdebea
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-20 17:56:19 +0000

    Fixed shuffle function

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -7,7 +7,6 @@ import numpy as np
 def shuffle_data(X, Y):
     """Function that suffles the data poins in two matrices
        the same way"""
-    X_suffle = X[np.random.permutation(X.shape[0])]
-    np.random.seed(0)
-    Y_suffle = Y[np.random.permutation(X.shape[0])]
-    return X_suffle, Y_suffle
+    m = X.shape[0]
+    shuff = np.random.permutation(m)
+    return X[shuff], Y[shuff]
