af5fa300e7021e85dc80a41276f4d657c6dbf405
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-20 17:39:35 +0000

    Fixed pycodestyle

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -5,10 +5,11 @@
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """Function that updates a variable in place using the Adam
        optimization algorithm"""
+
     Vdw = (beta1 * v) + ((1 - beta1) * grad)
     Sdw = (beta2 * s) + ((1 - beta2) * (grad ** 2))
     Vdw_co = Vdw / (1 - beta1 ** t)
     Sdw_co = Sdw / (1 - beta2 ** t)
-    W = var - (alpha * (Vdw_co / (Sdw_co **(1/2) + epsilon)))
+    W = var - (alpha * (Vdw_co / (Sdw_co ** (1/2) + epsilon)))
 
     return W, Vdw, Sdw
