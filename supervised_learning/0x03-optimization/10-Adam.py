f3dc91b4d453856f99670cf405660b089116c108
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-20 17:41:31 +0000

    Added import numpy

--- supervised_learning/0x03-optimization/10-Adam.py
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -2,12 +2,13 @@
 """Adam"""
 
 import tensorflow as tf
+import numpy as np
 
 
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """Function that creates the training operation for a neural network
        in tensorflow using the Adam optimization algorithm"""
     Optimizer = tf.train.AdamOptimizer(learning_rate=alpha, beta1=beta1,
-                                        beta2=beta2, epsilon=epsilon)
+                                       beta2=beta2, epsilon=epsilon)
     Minim = Optimizer.minimize(loss)
     return Minim
