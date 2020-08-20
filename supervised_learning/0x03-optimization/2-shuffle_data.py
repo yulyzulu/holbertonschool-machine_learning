cdb6776463e9feb69141a55f64728f09b0478818
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-20 17:29:37 +0000

    Permutatio

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -7,7 +7,7 @@ import numpy as np
 def shuffle_data(X, Y):
     """Function that suffles the data poins in two matrices
        the same way"""
-    X_suffle = np.random.permutation(X)
+    X_suffle = X[np.random.permutation(X.shape[0])]
     np.random.seed(0)
-    Y_suffle = np.random.permutation(Y)
+    Y_suffle = Y[np.random.permutation(X.shape[0])]
     return X_suffle, Y_suffle
