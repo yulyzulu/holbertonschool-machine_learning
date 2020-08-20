5ccfab311c8c9937c500bce246a116a8f976cffe
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-20 18:37:31 +0000

    Update

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -9,7 +9,7 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
         timedecay in numpy """
 
     if global_step % decay_rate == 0:
-        alpha = alpha / (1 + decay_rate * np.floor(global_step / decay_step))
+        alpha = alpha / (1 + decay_rate * (np.floor(global_step / decay_step)))
         return alpha
     else:
         return alpha
