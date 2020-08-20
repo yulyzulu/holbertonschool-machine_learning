53b42ba0eba028cc60ec8381fbb906cadedd589b
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-20 17:47:12 +0000

    Fixed pycodestyle

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -7,11 +7,8 @@ import numpy as np
 def batch_norm(Z, gamma, beta, epsilon):
     """Function that normalizes an unactivated output of a neural
         network using batch normalization"""
-    mean= Z.mean(axis=0)
+    mean = Z.mean(axis=0)
     var = Z.var(axis=0)
-    m = Z.shape[0]
-#    mean = (1/m) * np.sum(Z)
-#    var = (1/m) * np.sum((Z - mean)**2)
 
     Znor = (Z - mean) / ((var + epsilon)**(1/2))
 
