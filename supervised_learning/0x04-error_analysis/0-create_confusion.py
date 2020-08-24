ff0d21d7fcb46eb11cd45102bf8b2afe773fb252
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-24 18:21:45 +0000

    Added confusion  matrix

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Confusion matrix"""
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """Function that creates a confusion matrix"""
+    L = labels.shape[1] 
+    result = np.zeros((L, L))
+    result = np.matmul(labels.T,logits)
+
+    return result
