8468865aa5cc65d8764b524d2fce8d3e4bb432a2
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-26 23:57:03 +0000

    Added dropout function

--- supervised_learning/0x05-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -5,4 +5,9 @@ import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob):
-    
+    """ Function that """
+    W = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    keep_prob = tf.layers.Dropout(keep_prob)
+    dropout = tf.layers.Dense(n, activation, kernel_initializer=W,
+                              kernel_regularizer=keep_prob)
+    return dropout(prev)
