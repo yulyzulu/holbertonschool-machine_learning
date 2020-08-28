e6c1e68ff46120d3225a66cf0d94d2781df6f3cd
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-28 00:19:06 +0000

    Added early stopping

--- supervised_learning/0x05-regularization/7-early_stopping.py
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 """Early Stopping"""
 
+
 def early_stopping(cost, opt_cost, threshold, patience, count):
     """Function that determines if you should stop gradient descent early"""
+    dif = opt_cost - cost
+    val = True
+    if dif > threshold:
+        count = 0
+    else:
+        count = count + 1
+    if count != patience:
+        val = False
+    return val, count
