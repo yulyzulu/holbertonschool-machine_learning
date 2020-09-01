d39911027405c914db8d8ebfbf8c08ca8de43637
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-09-01 15:28:03 +0000

    Delete early condition

--- supervised_learning/0x06-keras/7-train.py
+++ supervised_learning/0x06-keras/7-train.py
@@ -10,7 +10,7 @@ def train_model(network, data, labels, batch_size, epochs,
                 decay_rate=1, verbose=True, shuffle=False):
     """Function to also train the model using learning decay"""
     callback_list = []
-    if validation_data and early_stopping:
+    if validation_data:
         early = K.callbacks.EarlyStopping(monitor="val_loss", mode='min',
                                           patience=patience)
         callback_list.append(early)
@@ -21,8 +21,6 @@ def train_model(network, data, labels, batch_size, epochs,
             lr = K.callbacks.LearningRateScheduler(schedule=scheduler,
                                                    verbose=1)
             callback_list.append(lr)
-#    else:
-#        early = None
 
     if len(callback_list) == 0:
         callback_list = None
