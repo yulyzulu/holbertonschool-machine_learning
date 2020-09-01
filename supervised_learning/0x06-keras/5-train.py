7877f219d38715f198f1ccf479d2de50587b77c0
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-09-01 14:34:12 +0000

    Added 5

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 """Train"""
+
 import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size,
                 epochs, validation_data=None,
                 verbose=True, shuffle=False):
-    """Function to also analyze validaiton data"""
-    history = network.fit(x=data, y=labels, batch_size=batch_size,
-                          epochs=epochs, verbose=verbose, shuffle=shuffle,
-                          validation_data=validation_data)
+    """Function that to also analyze validaiton data"""
+    history = network.fit(
+                        x=data,
+                        y=labels,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        verbose=verbose,
+                        shuffle=shuffle,
+                        validation_data=validation_data)
     return history
