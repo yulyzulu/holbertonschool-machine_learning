d8c4595b65d33470af5f4af683130fad207d9aba
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-09-01 13:54:12 +0000

    Added early condition and delete .history

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -8,13 +8,15 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, verbose=True, shuffle=False):
     """Function to also train the model using early stopping"""
-    if validation_data:
-        early = [K.callbacks.EarlyStopping(monitor="val_loss", mode='min',
-                                           patience=patience)]
+    callbacks_list = []
+    if validation_data and early_stopping:
+        early = K.callbacks.EarlyStopping(monitor="val_loss", mode='min',
+                                           patience=patience)
+        callbacks_list.append(early)
     else:
         early = None
 
     history = network.fit(x=data, y=labels, epochs=epochs, shuffle=shuffle,
                           batch_size=batch_size, verbose=verbose,
                           validation_data=validation_data, callbacks=early)
-    return history.history
+    return history
