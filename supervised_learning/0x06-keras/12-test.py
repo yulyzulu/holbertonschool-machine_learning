afbfe7b73b082feb227878d8ce95aa4daaf208d0
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-09-01 15:48:26 +0000

    Added test

--- supervised_learning/0x06-keras/12-test.py
+++ supervised_learning/0x06-keras/12-test.py
@@ -5,5 +5,4 @@ import tensorflow.keras as K
 
 def test_model(network, data, labels, verbose=True):
     """Function that tests a neural network"""
-    test = network.evaluate(x=data, y=labels, verbose=verbose)
-    return test
+    return network.evaluate(x=data, y=labels, verbose=verbose)
