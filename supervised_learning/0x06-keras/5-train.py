6cd50f9931d0d70cc8ec2d98045db03f8f0c73f8
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-09-01 14:04:27 +0000

    Change order function

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -7,6 +7,6 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
     """Function to also analyze validaiton data"""
     history = network.fit(x=data, y=labels, batch_size=batch_size,
-                          epochs=epochs, verbose=verbose, shuffle=shuffle
+                          epochs=epochs, verbose=verbose, shuffle=shuffle,
                           validation_data=validation_data)
     return history
