05818e05b059765457424fc6f8d62048f61dbb61
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-09-03 19:31:44 +0000

    Delete math import

--- math/0x04-convolutions_and_pooling/6-pool.py
+++ math/0x04-convolutions_and_pooling/6-pool.py
@@ -2,7 +2,6 @@
 """ Convolution with padding"""
 
 import numpy as np
-from math import floor, ceil
 
 
 def pool(images, kernel_shape, stride, mode='max'):
@@ -16,12 +15,11 @@ def pool(images, kernel_shape, stride, mode='max'):
     filter_w = kernel_shape[1]
     s1 = stride[0]
     s2 = stride[1]
-    n_dim1 = int(floor(float(image_h - filter_h + 1) / float(stride[0])))
-    n_dim2 = int(floor(float(image_w - filter_w + 1) / float(stride[1])))
+    n_dim1 = int((image_h - filter_h) / stride[0]) + 1
+    n_dim2 = int((image_w - filter_w) / stride[1]) + 1
     pool = np.zeros((m, n_dim1, n_dim2, nc))
     new_images = images.copy()
-#    new_images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
-#                        mode='constant')
+
     for x in range(n_dim1):
         for y in range(n_dim2):
             mini_matrix = new_images[:, x * s1: x * s1 + filter_h,
