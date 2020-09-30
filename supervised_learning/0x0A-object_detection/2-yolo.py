593a1ad0ddc05e7831591a1d6a7d8e38798b44c7
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-09-30 20:43:13 +0000

    Added task

---
+++ supervised_learning/0x0A-object_detection/2-yolo.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+""" Yolo Class """
+
+import tensorflow as tf
+import tensorflow.keras as K
+import numpy as np
+
+
+class Yolo():
+    """Yolo class"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """Class constructor"""
+        self.model = K.models.load_model(model_path)
+        with open(classes_path) as f:
+            self.class_names = []
+            for i in f:
+                i = i.split("\n")
+                self.class_names.append(i[0])
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def sigmoid(self, x):
+        """Sigmoid"""
+        return 1 / (1 + np.exp(-x))
+
+    def process_outputs(self, outputs, image_size):
+        """ Process_outpus method- Returns a tuple of
+          (boxes, box_confidences, box_class_probs)"""
+        boxes = []
+        box_class = []
+        box_confidences = []
+        i = 0
+        for output in outputs:
+            boxes.append(output[:, :, :, 0:4])
+            box_class.append(self.sigmoid(output[:, :, :, 5:]))
+            box_confidences.append(self.sigmoid(output[:, :, :, 4:5]))
+
+        for box in boxes:
+            H_box = box.shape[0]
+            W_box = box.shape[1]
+            anchor_box = box.shape[2]
+
+            the_box = np.zeros((H_box, W_box, anchor_box))
+
+            ind_x = np.arange(W_box)
+            ind_y = np.arange(H_box)
+            ind_x = ind_x.reshape(W_box, 1, 1)
+            ind_y = ind_y.reshape(H_box, 1, 1)
+
+            box_x = the_box + ind_x
+            box_y = the_box + ind_y
+
+            tx = box[..., 0]
+            ty = box[..., 1]
+            tw = box[..., 2]
+            th = box[..., 3]
+
+            sig_tx = self.sigmoid(tx)
+            sig_ty = self.sigmoid(ty)
+
+            bx = sig_tx + box_x
+            by = sig_ty + box_y
+            bx = bx / W_box
+            by = by / H_box
+
+            pw = self.anchors[i, :, 0]
+            ph = self.anchors[i, :, 1]
+
+            bw = pw * np.exp(tw)
+            bh = ph * np.exp(th)
+
+            inp_w = self.model.input.shape[1].value
+            inp_h = self.model.input.shape[2].value
+
+            bw = bw / inp_w
+            bh = bh / inp_h
+
+            x1 = bx - bw / 2
+            y1 = by - bh / 2
+            x2 = x1 + bw
+            y2 = y1 + bh
+
+            box[..., 0] = x1 * image_size[1]
+            box[..., 1] = y1 * image_size[0]
+            box[..., 2] = x2 * image_size[1]
+            box[..., 3] = y2 * image_size[0]
+            i = i + 1
+
+        return (boxes, box_confidences, box_class)
+
+    def filter_boxes(self, boxes, box_confidences, box_class_probs):
+        """Filter_boxes method- Return a tuple of
+           (filtered_boxes, box_classes, box_scores)"""
+        fil_scores = []
+        fil_boxes = []
+        fil_classes = []
+        scores = []
+        for i in range(len(boxes)):
+            box_scores = box_confidences[i] * box_class_probs[i]
+            box_classes = np.argmax(box_scores, axis=-1)
+            box_class_scores = np.max(box_scores, axis=-1)
+            filter_mask = box_class_scores >= self.class_t
+            scores = filter_mask * box_class_scores
+            scores = scores[scores > 0]
+            classes = filter_mask * box_classes
+            classes = classes[classes > 0]
+            #boxes_list = boxes[i] * filter_mask
+            #filter_reshape = filter_mask((boxes[i].shape), 1)
+            #boxes_list = boxes[i] * filter_reshape
+            #boxes_list = boxes_list[boxes_list != 0]
+            boxes_list = boxes[i].reshape(-1, 4)
+            #boxes_list = boxes_list[filter_mask]
+            fil_scores.append(scores)
+            fil_boxes.append(boxes_list)
+            #print(fil_boxes)
+            fil_classes.append(classes)
+        fil_scores = np.concatenate(fil_scores)
+        fil_boxes = np.concatenate(fil_boxes, axis=0)
+        fil_classes = np.concatenate(fil_classes)
+        return (fil_boxes, fil_classes, fil_scores)
