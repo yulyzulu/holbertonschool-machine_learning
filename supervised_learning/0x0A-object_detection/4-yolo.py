42559d6718ba774177ed33f0011febecfe426f28
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-10-01 21:21:14 +0000

    with grob

--- supervised_learning/0x0A-object_detection/4-yolo.py
+++ supervised_learning/0x0A-object_detection/4-yolo.py
@@ -163,16 +163,17 @@ class Yolo():
     @staticmethod
     def load_images(folder_path):
         """Static method to load image"""
-        content = os.listdir(folder_path)
-        #path = glob.glob(folder_path + "/*.jpg")
+        #content = os.listdir(folder_path)
+        path = glob.glob(folder_path + "/*.jpg")
         #print(path)
         #print(content)
         images = []
-        images_path = []
-        for i in content:
+        #images_path = []
+        for i in path:
             image = cv2.imread(i)
             #image1 = cv2.cvtColor(image, COLOR_BGR2RGB)
             #cv2.imwrite(i, image)
             images.append(image)
-            images_path.append(folder_path + '/' + i)
-        return images_path, images
+            #images_path.append(folder_path + '/' + i)
+        #print(images_path)
+        return path, images
